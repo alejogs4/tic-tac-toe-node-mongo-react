a0e95c632c3ab8b230ba0dbf17a756868ae9f9f3
alejogs4 <alejogs4@gmail.com>
2018-12-30 23:32:42 -0500

    Remove files from src folder

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import AppRoutes from './AppRoutes';
-import {BrowserRouter} from 'react-router-dom'
+import { BrowserRouter } from 'react-router-dom'
 
 ReactDOM.render(
   <BrowserRouter>
